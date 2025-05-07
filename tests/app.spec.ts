9e178f490948e89bdbaeff9170cde8ccc3da3ac1
abdessittir <harkati.web.dev@gmail.com>
2025-05-07 11:50:56 +0100

    Add app tests

---
+++ tests/app.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Vite + React Hello/);
+});
+
+test('should fails', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/ok/);
+});
