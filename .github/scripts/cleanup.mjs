ad62ab2daa42feb886aea6997e50b22ac89b2ca4
abdessittir <harkati.web.dev@gmail.com>
2025-05-08 09:23:10 +0100

    Add Trace

---
+++ .github/scripts/cleanup.mjs
@@ -0,0 +1,4 @@
+import { getOctokit, context } from '@actions/github';
+import * as core from '@actions/core';
+import process from 'process';
+
--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: process.env.CI ? 'on' : 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
