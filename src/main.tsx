1b1182e2e4ae8a54d277bba9c7c7706cf45f10a6
Abdessittir Harkati <harkati.web.dev@gmail.com>
2025-12-30 19:14:28 +0100

    Add console log for debugging

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log('ok 2');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
